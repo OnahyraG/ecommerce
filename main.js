692427dbc4fc03d59ac7d0e5e7d42ba72c67ceed
Onahyra Guerrero <guerreromendivelso@gmail.com>
2022-06-22 22:37:05 -0300

    separar

--- main.js
+++ main.js
@@ -1,63 +1,7 @@
-const BBDD = [
-    {
-        "id": 1,
-        "nombre": "belleza de manos",
-        "img": "imagenes/spa/belleza de manos.png",
-        "precio": 600,
-        "cantidad":1,
-        "descripcion": " Limpieza de uña( esmalte, cuticula ). Exfoliacion para retirar celulas muertas y mascara de fango para aclarar la piel y generar celulas madres",
-    },
-    {
-        "id": 2,
-        "nombre": "esmaltado tradicional",
-        "img": "imagenes/spa/esmaltado tradicional.jpg",
-        "precio": 500,
-        "cantidad":1,
-        "descripcion": "Nunca pasara de moda. Secado al aire, facil desmaquillado",
-    },
-    {
-        "id": 3,
-        "nombre": "esmaltado semipermanente",
-        "img": "imagenes/spa/esmaltado semi.webp",
-        "precio": 800,
-        "cantidad":1,
-        "descripcion": " El servis tiene una duracion de 2 a 3 semanas aproximadamente, la capa es un poco mas gruesa que la tradicional proporcionando mas cobertura de la uña",
-
-    },
-    {
-        "id": 4,
-        "nombre": "esmaltado semi + baño en acrilico",
-        "img": "imagenes/spa/baño en acrilico.png",
-        "precio": 1000,
-        "cantidad": 1,
-        "descripcion":" El baño en acrilico consiste en aplicar el polvo altes del esmaltado, creando una capa un poco mas dura para proteccion de la uña",
-
-    },
-    {
-        "id": 5,
-        "nombre": " kappig de uñas",
-        "img": "imagenes/spa/kapping.jpg",
-        "precio": 1300,
-        "cantidad": 1,
-        "descripcion":" Antes del esmaltado semi se prepara la uña con 2 capaz finas de gel para crear una coraza del tamaño de tu uña, este servis requiere de cuidado continuo",
-
-    },
-    {
-        "id": 6,
-        "nombre": "uña esculpida en gel",
-        "img": "imagenes/spa/esculpida.jpg",
-        "precio": 1600,
-        "cantidad": 1,
-        "descripcion":"Las uñas de tu sueño, desde estilo ROSALIA hasta estilo oficina, con este metodo podemos alargar y darle forma a la uña a gusto",
-
-    }
-]
 
 const carrito = [];
 
 let total = 0;
-
-let usuario = prompt("BIENVENIDO. ingresa tu nombre");
 
 
 
